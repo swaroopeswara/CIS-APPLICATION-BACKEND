724e7a70ee3d0f17fe989fde1f5b1141c76747b6
Swaroop Eswara <raghava.eswara@mmiholdings.co.za>
2019-04-03 21:17:52 +0200

    Tested the service update dto

---
+++ src/main/java/com/dw/ngms/cis/uam/dto/UpdatePasswordDTO.java
@@ -0,0 +1,27 @@
+package com.dw.ngms.cis.uam.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by swaroop on 2019/04/03.
+ */
+
+@Data
+@Getter
+@Setter
+@ToString
+public class UpdatePasswordDTO implements Serializable {
+
+    private String usercode;
+    private String username;
+    private String oldpassword;
+    private String newpassword;
+
+
+}
