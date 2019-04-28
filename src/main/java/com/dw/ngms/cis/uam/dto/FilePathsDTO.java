4a90766b456cf01159155a78d655c95bf7fb545f
Swaroop Eswara <raghava.eswara@mmiholdings.co.za>
2019-04-28 13:59:10 +0200

    Files search service

---
+++ src/main/java/com/dw/ngms/cis/uam/dto/FilePathsDTO.java
@@ -0,0 +1,14 @@
+package com.dw.ngms.cis.uam.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by swaroop on 2019/04/26.
+ */
+@Getter
+@Setter
+public class FilePathsDTO {
+
+    String[] files;
+}
