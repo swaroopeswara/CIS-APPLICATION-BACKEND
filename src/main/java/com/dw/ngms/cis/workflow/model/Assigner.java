40913f1c3d65d63ebd50d37884fcf53e7ca7322b
dataworld <dataworldproject@bitbucket.org>
2019-05-17 01:41:37 +0200

    adding missing assigner class

---
+++ src/main/java/com/dw/ngms/cis/workflow/model/Assigner.java
@@ -0,0 +1,25 @@
+package com.dw.ngms.cis.workflow.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@XmlRootElement
+public class Assigner {
+
+	@JsonProperty("type")
+	private String type;
+	@JsonProperty("name")
+	private String name;
+}
